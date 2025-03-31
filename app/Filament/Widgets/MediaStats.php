816c42071717e8b5062571367d9142004329809c
silasmas <silasjmas@gmail.com>
2025-03-31 15:33:51 +0100

    Update MediaStats.php

--- app/Filament/Widgets/MediaStats.php
+++ app/Filament/Widgets/MediaStats.php
@@ -2,9 +2,8 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Media;
-use Filament\Widgets\StatsOverviewWidget\Card;
-use Filament\Widgets\StatsOverviewWidget\Stat;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class MediaStats extends BaseWidget
 {
@@ -14,26 +13,26 @@ class MediaStats extends BaseWidget
     protected static ?int $sort    = 0; // Pour l'afficher après les stats globales
     protected function getCards(): array
     {
-        $total      = Media::count();
-        $avg = Media::avg('price');
+        $total = Media::count();
+        $avg   = Media::avg('price');
         return [
             Stat::make('Total Médias', $total)
                 ->description('Nombre total de médias')
                 ->icon('heroicon-o-film')
                 ->color('primary'),
-                Stat::make('Prix moyen', number_format($avg, 2) . ' FCFA')
+            Stat::make('Prix moyen', number_format($avg, 2) . ' FCFA')
                 ->description('Prix moyen de tous les médias')
                 ->icon('heroicon-o-currency-dollar')
                 ->color('success'),
-                Stat::make('Médias en live', Media::where('is_live', true)->count())
+            Stat::make('Médias en live', Media::where('is_live', true)->count())
                 ->description('Contenus actuellement en direct')
                 ->icon('heroicon-o-bolt')
                 ->color('warning'),
-                Stat::make('Médias publics', Media::where('is_public', 'public')->count())
+            Stat::make('Médias publics', Media::where('is_public', true)->count())
                 ->color('primary')
                 ->icon('heroicon-o-eye'),
 
-            Stat::make('Médias privés', Media::where('is_public', 'private')->count())
+            Stat::make('Médias privés', Media::where('is_public', false)->count())
                 ->color('danger')
                 ->icon('heroicon-o-eye-slash'),
         ];
