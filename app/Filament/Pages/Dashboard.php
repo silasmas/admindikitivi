3aa49aa3441359aba2cf6762a2003640a91307a1
silasmas <silasjmas@gmail.com>
2025-04-01 09:48:20 +0100

    ggf

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -2,9 +2,19 @@
 
 namespace App\Filament\Pages;
 
+
+use App\Filament\Widgets\GenderStatsWidget;
 use Schmeits\FilamentUmami\Concerns\HasFilter;
 
 class Dashboard extends \Filament\Pages\Dashboard
 {
     use HasFilter;
+    public function getWidgets(): array
+{
+    return [
+
+        // ... autres widgets
+    ];
+}
+
 }
